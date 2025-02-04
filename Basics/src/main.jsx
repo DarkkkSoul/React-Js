4a92dd600b0b7c741bd67ad736feacea4bc5f558
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-02-04 13:42:54 +0530

    how react stores an element

--- Basics/src/main.jsx
+++ Basics/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React, { StrictMode } from 'react';          // Correct import for React and StrictMode
+import { createRoot } from 'react-dom/client';      // Correct import for createRoot
 import App from './App.jsx';
+const reactElem = React.createElement(
+  'a',
+  {
+    href:'https://youtube.com',
+    target:'_blank'
+  },
+  'visit youtube here'
+);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    {reactElem}
   </StrictMode>
 );
 
