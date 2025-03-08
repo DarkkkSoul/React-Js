2ad4a78cf0c164033314c5effa89755b66168ae7
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-03-08 15:21:20 +0530

    theme switcher using context api

--- contextAPI2/src/App.jsx
+++ contextAPI2/src/App.jsx
@@ -1,12 +1,39 @@
-import { useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
+import Button from './components/Button'
+import { useEffect, useState } from 'react';
+import Card from './components/Card';
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [themeMode, setThemeMode] = useState('light');
+
+    const darkTheme = () => {
+        setThemeMode('dark');
+    }
+    const lightTheme = () => {
+        setThemeMode('light');
+    }
+
+    useEffect(() => {
+        document.querySelector('html').classList.remove("light", "dark")
+        document.querySelector('html').classList.add(themeMode)
+    }, [themeMode])
 
     return (
         <>
+            <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+                <div className="flex flex-wrap min-h-screen items-center">
+                    <div className="w-full">
+                        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                            <Button />
+                        </div>
 
+                        <div className="w-full max-w-sm mx-auto">
+                            < Card />
+                        </div>
+                    </div>
+                </div>
+            </ThemeProvider>
         </>
     )
 }
--- contextAPI2/src/contexts/theme.js
+++ contextAPI2/src/contexts/theme.js
@@ -1,15 +1,15 @@
 import { createContext, useContext } from "react";
 
+// till here same as UserContext.js , can pass values here directly
 export const ThemeContext = createContext({
-    themeMode: 'Light',
+    themeMode: 'light',
     darkTheme: () => { },
     lightTheme: () => { },
 });
-// till here same as UserContext.js , can pass values here directly
 
 //next convert ThemeContext into provider
 
-export const ThemeProvider = ThemeContext.Provider();
+export const ThemeProvider = ThemeContext.Provider;
 
 //now call context, useContext();
 export default function useTheme() {
